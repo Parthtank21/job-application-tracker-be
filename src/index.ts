6a8b42f0b18840bf66dc569a493784343f0583a0
Parth Tank <parthtank301@gmail.com>
2025-02-23 00:02:44 +0530

    FEAT: completed mongo database setup

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import dotenv from "dotenv";
 
 import app from "./app";
 import logger from "./utils/logger.utils";
+import connectDb from "./utils/db.utils";
 
 dotenv.config();
 
 const port = process.env.PORT || 3001;
+const dbUri = process.env.MONGO_URI as string;
 
 const start = async () => {
   try {
+    await connectDb(dbUri);
     app.listen(port, () =>
       logger.info(`Server is listening on port ${port}...`)
     );
